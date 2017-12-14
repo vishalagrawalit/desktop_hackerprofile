916ca781908503f8026425f16b7e3071750cebdb
vishvishu <vishvishu14@gmail.com>
2017-12-14 23:13:36 +0530

    Place in your username so that it can scrape your details.

---
+++ usernames.py
@@ -0,0 +1,7 @@
+# Replace Enter your username with your respective usernames, so that we can scrap the data.
+
+codechef_username = "Enter your Username."
+codeforces_username = "Enter your Username."
+hackerEarth_username = "Enter your Username."
+hackerRank_username = "Enter your Username."
+spoj_username = "Enter your Username."
