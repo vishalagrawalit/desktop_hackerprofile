67266e31b6a554aeb6cfd675121c38631b027b35
vishvishu <vishvishu14@gmail.com>
2017-12-15 05:45:42 +0530

    Bugs fixed!

--- web.py
+++ web.py
@@ -5,21 +5,31 @@ from usernames import codechef_username, codeforces_username, hackerEarth_userna
 icon_path = "/home/vish/Downloads/index.png"
 
 # Extracting Details
-cc = codechef(codechef_username)
-cf = codeforces(codeforces_username)
-he = hackerearth(hackerEarth_username)
-hr = hackerrank(hackerRank_username)
-sp = spoj(spoj_username)
-
+user = "Enter your Username."
 result = ""
-result += "Codechef:\n Problems Solved: " + str(cc[0]) + "\n Current Rating: " + str(cc[1])
-result += "\nCodeForces:\n Contest Played: " + str(cf[0]) + "\n Current Rating: " + str(cf[1])
-result += "\nHackerEarth:\n Problems Solved: " + str(he[0]) + "\n Current Rating: " + str(he[1])
-result += "\nHackerRank:\n Problems Solved: " + str(hr[0]) + "\n Current Rating: " + str(hr[1])
-result += "\nSPOJ:\n Problems Solved: " + str(sp[0]) + "\n Current Rating: " + str(sp[1])
+total = 0
+
+if codechef_username != user:
+    cc = codechef(codechef_username)
+    result += "Codechef:\n Problems Solved: " + str(cc[0]) + "\n Current Rating: " + str(cc[1])
+    total += cc[0]
+if codeforces_username != user:
+    cf = codeforces(codeforces_username)
+    result += "\nCodeForces:\n Contest Played: " + str(cf[0]) + "\n Current Rating: " + str(cf[1])
+if hackerEarth_username != user:
+    he = hackerearth(hackerEarth_username)
+    result += "\nHackerEarth:\n Problems Solved: " + str(he[0]) + "\n Current Rating: " + str(he[1])
+    total += he[0]
+if hackerRank_username != user:
+    hr = hackerrank(hackerRank_username)
+    result += "\nHackerRank:\n Problems Solved: " + str(hr[0]) + "\n Current Rating: " + str(hr[1])
+    total += hr[0]
+if spoj_username != user:
+    sp = spoj(spoj_username)
+    result += "\nSPOJ:\n Problems Solved: " + str(sp[0]) + "\n Current Rating: " + str(sp[1])
+    total += sp[0]
 
 result += "\nTotal Problems: "
-total = cc[0] + he[0] + hr[0] + sp[0]
 result += str(total)
 
 notify2.init("HackerProfile Notifier")
