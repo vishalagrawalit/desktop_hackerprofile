246e03f8dc4da0d06f37d5063cb332d504621375
vishvishu <vishvishu14@gmail.com>
2017-12-15 05:41:25 +0530

    Comments Updated

--- usernames.py
+++ usernames.py
@@ -1,4 +1,5 @@
 # Replace Enter your username with your respective usernames, so that we can scrap the data.
+# If you are not a member in any of the websites, please don't change anything.
 
 codechef_username = "Enter your Username."
 codeforces_username = "Enter your Username."
